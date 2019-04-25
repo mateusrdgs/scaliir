450cdb94bb5f5a5b7ad2b2e69b25f790ecd87cd2
Mateus Rodrigues <devrdgs@gmail.com>
2019-04-25 14:08:13 -0300

    chore: created src folder

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+const App = () => {
+  return (
+    <div>Hello world</div>
+  )
+};
+
+ReactDOM.render(App, rootEl);
