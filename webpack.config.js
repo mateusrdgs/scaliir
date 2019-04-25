1ea0a9c83464ef50e11c329723728f499912f23d
Mateus Rodrigues <devrdgs@gmail.com>
2019-04-25 12:36:15 -0300

    chore: added some webpack basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
