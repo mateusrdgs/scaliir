a7a706aca49e1720232c50b0370c8f5b2eec94fc
Mateus Rodrigues <devrdgs@gmail.com>
2019-05-12 14:10:14 -0300

    chore: generating files with hash

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,35 +1,33 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '../', 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './template/index.html',
-    }),
-    new webpack.ProvidePlugin({
-      'React': 'react'
-    })
-  ]
-}
+	entry: './src/index.js',
+	output: {
+		filename: 'index.[hash].js',
+		path: path.resolve(__dirname, '../', 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+		],
+	},
+	resolve: {
+		alias: {
+			'react-dom': '@hot-loader/react-dom',
+		},
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './template/index.html',
+		}),
+		new webpack.ProvidePlugin({
+			React: 'react',
+		}),
+	],
+};
