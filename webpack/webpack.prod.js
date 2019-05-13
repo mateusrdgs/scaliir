408bd2604fa8e974b2b1d2271ddc9559a8048673
Mateus Rodrigues <devrdgs@gmail.com>
2019-05-13 13:14:46 -0300

    chore: added uglifyjs plugin to project

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,25 +3,31 @@ const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
-	mode: 'production',
-	plugins: [
-		new CleanWebpackPlugin(),
-		new webpack.EnvironmentPlugin({
-			NODE_ENV: 'production',
-		}),
-	],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendor',
-					chunks: 'all',
-				},
-			},
-		},
-		minimizer: [new OptimizeCSSAssetsPlugin()]
-	}
+  mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin({
+        parallel: true
+      })
+    ]
+  }
 });
