72ab97b6e09cdb05affef1e9f5ecaf6076f56c60
Mateus Rodrigues <devrdgs@gmail.com>
2019-05-13 12:11:19 -0300

    chore: outputing files with [name].[hash]

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'index.[hash].js',
+		filename: '[name].[hash].js',
 		path: path.resolve(__dirname, '../', 'dist'),
 	},
 	module: {
