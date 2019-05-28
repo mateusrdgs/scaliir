a6fa7cc94070002b08522f04ac0ccb19ee7a7a82
Mateus Rodrigues <devrdgs@gmail.com>
2019-05-27 22:31:38 -0300

    chore: rendering home page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 
+import RootComponent from 'components/RootComponent';
 import routes from './routes';
-import RootComponent from './components/RootComponent';
 
 const rootEl = document.getElementById('root');
 const rootComponentProps = {
   routes,
 };
 
+
 const render = Component => ({ ...props }) => {
-  ReactDOM.render(
-    <Component {...props} />,
-    rootEl,
-  );
+  ReactDOM.render(<Component {...props} />, rootEl);
 };
 
 render(RootComponent)({ ...rootComponentProps });
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept('./routes', () => {
-    // eslint-disable-next-line global-require
     const nextPropsRoutes = require('./routes').default;
     const nextProps = {
       ...rootComponentProps,
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-
 const Home = () => {
-  const [counter, setCounter] = useState(0);
   return (
     <div className="home">
       Home
-      <button type="button" onClick={() => setCounter(counter + 1)}>{counter}</button>
     </div>
   );
 };
