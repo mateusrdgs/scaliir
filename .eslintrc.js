6e58b76cabea045d473cf934ca40c68ff536efb1
Mateus Rodrigues <devrdgs@gmail.com>
2019-06-03 22:38:37 -0300

    chore: simplified eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true
   },
-  root: false,
-  extends: 'airbnb',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: 'babel-eslint',
+  extends: [
+    "react-app"
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    allowImportExportEverywhere: true,
     sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
-    'import/no-unresolved': 0,
-    'arrow-body-style': 0,
-    'global-require': 0,
-    'react/jsx-filename-extension': 0,
-    'react/react-in-jsx-scope': 0,
-    'react/prop-types': 0,
-    'import/no-extraneous-dependencies': 0
+    'react/react-in-jsx-scope': 'off',
   },
 };
