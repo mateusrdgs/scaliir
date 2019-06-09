4c75bba875258eb7b0340a5d8a5e273591fe03cd
Mateus Rodrigues <devrdgs@gmail.com>
2019-06-09 17:53:55 -0300

    feat: moved duplicated stylus code to config folder

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,11 @@
+import './home.styl'
+
 const Home = () => {
   return (
-    <div className="home">
-      Home
+    <div className='home'>
+      <img className='react-logo' src='assets/images/react.png' alt='react logo' />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
