23bb477773601c4673124dd41bace6c46a170ffa
Mateus Rodrigues <devrdgs@gmail.com>
2019-06-11 00:02:21 -0300

    feat: created pagenames related files

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -1,10 +1,18 @@
+import { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
+import AppLayout from 'layout/AppLayout'
+import FallbackLoader from 'elements/FallbackLoader'
 
 const RootComponent = ({ routes, store }) => {
   return (
     <Provider store={store}>
-      <BrowserRouter>{routes}</BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<FallbackLoader />}>
+          <AppLayout>{routes}</AppLayout>
+        </Suspense>
+      </BrowserRouter>
     </Provider>
   )
 }
---
+++ src/constants/pagenames.js
@@ -0,0 +1,5 @@
+import createPageNames from 'utils/createPagenames'
+
+const pageNames = createPageNames()
+
+export default pageNames
