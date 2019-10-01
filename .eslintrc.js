7e87c70b37b3a7b4602acccc778c48637214134c
Mateus Rodrigues <devrdgs@gmail.com>
2019-10-01 00:04:38 -0300

    fix: linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
   rules: {
-    'react/react-in-jsx-scope': 0,
-  },
+    'react/react-in-jsx-scope': 0
+  }
 }
