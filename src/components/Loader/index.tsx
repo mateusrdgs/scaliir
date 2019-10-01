3493a24cf8f68fc4648bd887b3ae3f7cd95a2617
Mateus Rodrigues <devrdgs@gmail.com>
2019-10-01 00:17:50 -0300

    chore: getting rid of elements folder

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,7 @@
+import './loader.styl';
+
+const Loader = (): React.ReactElement => {
+  return <div className='loader' />;
+};
+
+export default Loader;
--- src/components/RootComponent.tsx
+++ src/components/RootComponent.tsx
@@ -1,23 +1,21 @@
-import { Suspense } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { Store } from 'redux'
+import { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
 
-import AppLayout from 'layout/AppLayout'
-import FallbackLoader from 'elements/FallbackLoader'
+import AppLayout from 'layout/AppLayout';
+import FallbackLoader from 'components/FallbackLoader';
 
 const RootComponent = ({ routes, store }: { routes: any; store: Store }): React.ReactElement => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Suspense fallback={<FallbackLoader />}>
-          <AppLayout>
-            {routes}
-          </AppLayout>
+          <AppLayout>{routes}</AppLayout>
         </Suspense>
       </BrowserRouter>
     </Provider>
-  )
-}
+  );
+};
 
-export default RootComponent
+export default RootComponent;
